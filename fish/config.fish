d8244661e714016c737da806ebaf57a31d1fcda3
Bluesboy <youngreg@ya.ru>
2022-01-08 08:38:05 +0500

    Setup Starship and small refactor

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
+# Initialize Starship prompt
 starship init fish | source
 
-#Set Neovim as default editor
+# Set Neovim as default editor
 set -gx EDITOR nvim
 
-#Setup PATH from .bash_profile
-fish_add_path $PATH
+# Set Starship config directory compatible with new standards
+set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
+
+# Setup PATH from .bash_profile
+fish_add_path -m ~/.local/bin
