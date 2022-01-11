1244b19b438d14e65e5face950b0194867329d92
Bluesboy <youngreg@ya.ru>
2022-01-12 03:28:34 +0500

    Install pass-git-helper

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,6 +10,9 @@ set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
 # Set Starship config directory compatible with new standards
 set -gx N_PREFIX ~/.local/share/n
 
+# Set TTY for pass-git-helper
+set -gx GPG_TTY (tty)
+
 # Setup PATH from .bash_profile
 fish_add_path -m ~/.local/bin
 fish_add_path -m $N_PREFIX
