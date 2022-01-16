8e0f4153d51d879f2ccf12601f063d66bac1f73d
Bluesboy <youngreg@ya.ru>
2022-01-16 08:59:10 +0500

    Remove prompt configuration for fish since starship is used

--- config/fish/functions/fish_right_prompt.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /home/ssattarov/.config/fish/functions/right_prompt.fish @ line 2
-function fish_right_prompt
-  #intentionally left blank
-end
--- config/fish/functions/right_prompt.fish
+++
@@ -1,4 +0,0 @@
-#Remove Time from right side of screen
-function fish_right_prompt
-  #intentionally left blank
-end
