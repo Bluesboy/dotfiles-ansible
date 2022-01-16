a036daf184703cae411014363e2829988fcb25f2
Bluesboy <youngreg@ya.ru>
2022-01-16 10:29:17 +0500

    Set abbreviations in fish config file instead as universal variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
+#Use VI mode by default
+fish_vi_key_bindings
+
 # Initialize Starship prompt
 starship init fish | source
 
@@ -21,10 +24,32 @@ fish_add_path -m ~/.local/bin
 fish_add_path -m $N_PREFIX
 fish_add_path -m ~/.cargo/bin
 
-function fish_greeting
-  # Put this here to override default greeting message
-end
+# Do not display greeting
+functions -e fish_greeting
 
+# Bind lf start to CTRL+O
 function fish_user_key_bindings
   bind -M insert \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+end
+
+# Setup abbreviations
+if status --is-interactive
+  abbr --add --global -- aurox 'gcloud config configurations activate aurox'
+  abbr --add --global -- dr 'docker run -it --rm'
+  abbr --add --global -- flux 'gcloud config configurations activate flux'
+  abbr --add --global -- g git
+  abbr --add --global -- ga 'git add'
+  abbr --add --global -- gc 'git commit -m'
+  abbr --add --global -- gca 'git commit --amend'
+  abbr --add --global -- glo 'git lo'
+  abbr --add --global -- gp 'git push'
+  abbr --add --global -- gpu 'git pull'
+  abbr --add --global -- gs 'git status'
+  abbr --add --global -- hl 'helm ls'
+  abbr --add --global -- k kubectl
+  abbr --add --global -- kc kubectx
+  abbr --add --global -- kn kubens
+  abbr --add --global -- lg lazygit
+  abbr --add --global -- tf terraform
+  abbr --add --global -- fishconf 'vim ~/.config/fish/config.fish'
 end
