d130f24a331f49c6d6407b62828c97127483ef1a
Bluesboy <youngreg@ya.ru>
2022-05-18 19:06:53 +0500

    Fix Neovim plugins configuration

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -45,10 +45,10 @@ nvim_tree.setup({
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
-    update_to_buf_dir = {
-        enable = true,
-        auto_open = true,
-    },
+    -- update_to_buf_dir = {
+    --     enable = true,
+    --     auto_open = true,
+    -- },
     diagnostics = {
         enable = true,
         icons = {
@@ -73,7 +73,7 @@ nvim_tree.setup({
         height = 30,
         hide_root_folder = false,
         side = "left",
-        auto_resize = true,
+        -- auto_resize = true,
         mappings = {
             custom_only = false,
             list = {
@@ -87,15 +87,33 @@ nvim_tree.setup({
         number = false,
         relativenumber = false,
     },
-    quit_on_open = 0,
-    git_hl = 1,
-    disable_window_picker = 0,
-    root_folder_modifier = ":t",
-    show_icons = {
-        git = 1,
-        folders = 1,
-        files = 1,
-        folder_arrows = 1,
-        tree_width = 30,
+    -- git_hl = true,
+    -- root_folder_modifier = ":t",
+    -- show_icons = {
+    --     git = 1,
+    --     folders = 1,
+    --     files = 1,
+    --     folder_arrows = 1,
+    --     tree_width = 30,
+    -- },
+    actions = {
+      use_system_clipboard = true,
+      change_dir = {
+        enable = true,
+        global = false,
+        restrict_above_cwd = false,
+      },
+      open_file = {
+        quit_on_open = false,
+        resize_window = false,
+        window_picker = {
+          enable = true,
+          chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+          exclude = {
+            filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+            buftype = { "nofile", "terminal", "help" },
+          },
+        },
+      },
     },
 })
