439beef5bf2c396ff0fc5ba8c19d2ef536575ba3
Bluesboy <youngreg@ya.ru>
2023-09-02 08:32:46 +0500

    Fix github-theme settings

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,19 +1,24 @@
 require("github-theme").setup({
-    function_style = "italic",
-    sidebars = { "qf", "vista_kind", "terminal", "packer" },
-
-    -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-    colors = { hint = "orange", error = "#ff0000" },
-
-    -- Overwrite the highlight groups
-    overrides = function(c)
-        return {
-            htmlTag = { fg = c.red, bg = "#282c34", sp = c.hint, style = "underline" },
-            DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-            -- this will remove the highlight groups
-            TSField = {},
-        }
-    end,
+  options = {
+    styles = {
+      functions = 'italic'
+    },
+    darken = {
+      floats = false,
+      sidebars = {
+        enabled = true,
+        list = {
+          "qf",
+          "vista_kind",
+          "terminal",
+          "packer"
+        },
+      },
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
 })
 
 vim.cmd.colorscheme('github_dark')
