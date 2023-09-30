646ab766b622cdcc7dfc01f08e0021193ba7c4ec
Bluesboy <youngreg@ya.ru>
2023-09-30 08:43:44 +0500

    Update indent-blankline settings

--- config/nvim/lua/user/indent-blankline.lua
+++ config/nvim/lua/user/indent-blankline.lua
@@ -1,13 +1,36 @@
-local indent_blankline_status_ok, indent_blankline = pcall(require, "indent_blankline")
-if not indent_blankline_status_ok then
+local ibl_status_ok, ibl = pcall(require, "ibl")
+if not ibl_status_ok then
   return
 end
+
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
 
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
 
-indent_blankline.setup({
-  show_end_of_line = true,
-  show_current_context = true,
-  show_current_context_start = true,
+ibl.setup({
+  indent = {
+    highlight = highlight
+  }
 })
