e31aa436bf574111c99e9170eb0b2e46b0c4ebb7
Bluesboy <youngreg@ya.ru>
2024-05-05 10:15:37 +0500

    Refactor syntax

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,5 +1,3 @@
-local opts = { noremap = true, silent = true }
-
 local term_opts = { silent = true }
 
 -- Shorten function name
@@ -12,7 +10,7 @@ local function map(mode, lhs, rhs, opts)
 end
 
 --Remap space as leader key
-map("", "<Space>", "<Nop>", opts)
+map("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -26,43 +24,44 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 
-map("n", "<leader>sf", ":source %<cr>", opts)
+map("n", "<leader>sf", ":source %<cr>")
+
 -- Better window navigation
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
 
 -- Resize with arrows
-map("n", "<C-Up>", ":resize +2<cr>", opts)
-map("n", "<C-Down>", ":resize -2<cr>", opts)
-map("n", "<C-Left>", ":vertical resize -2<cr>", opts)
-map("n", "<C-Right>", ":vertical resize +2<cr>", opts)
+map("n", "<C-Up>", ":resize +2<cr>")
+map("n", "<C-Down>", ":resize -2<cr>")
+map("n", "<C-Left>", ":vertical resize -2<cr>")
+map("n", "<C-Right>", ":vertical resize +2<cr>")
 
 -- Navigate buffers
-map("n", "<S-l>", ":bnext<cr>", opts)
-map("n", "<S-h>", ":bprevious<cr>", opts)
+map("n", "<S-l>", ":bnext<cr>")
+map("n", "<S-h>", ":bprevious<cr>")
 
 -- Insert --
 -- Press jk fast to enter
-map("i", "jk", "<ESC>", opts)
+map("i", "jk", "<ESC>")
 
 -- Visual --
 -- Stay in indent mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- Move text up and down
-map("v", "<A-j>", ":m .+1<cr>==", opts)
-map("v", "<A-k>", ":m .-2<cr>==", opts)
-map("v", "p", '"_dP', opts)
+map("v", "<A-j>", ":m .+1<cr>==")
+map("v", "<A-k>", ":m .-2<cr>==")
+map("v", "p", '"_dP')
 
 -- Visual Block --
 -- Move text up and down
-map("x", "J", ":move '>+1<cr>gv-gv", opts)
-map("x", "K", ":move '<-2<cr>gv-gv", opts)
-map("x", "<A-j>", ":move '>+1<cr>gv-gv", opts)
-map("x", "<A-k>", ":move '<-2<cr>gv-gv", opts)
+map("x", "J", ":move '>+1<cr>gv-gv")
+map("x", "K", ":move '<-2<cr>gv-gv")
+map("x", "<A-j>", ":move '>+1<cr>gv-gv")
+map("x", "<A-k>", ":move '<-2<cr>gv-gv")
 
 -- Terminal --
 -- Better terminal navigation
@@ -72,33 +71,28 @@ map("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 map("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Leader write, quit and both
-map("n", "<leader>w", ":w<cr>", opts)
-map("n", "<leader>q", ":q<cr>", opts)
-map("n", "<leader>x", ":q!<cr>", opts)
-map("n", "<leader>Q", ":wq<cr>", opts)
+map("n", "<leader>w", ":w<cr>")
+map("n", "<leader>q", ":q<cr>")
+map("n", "<leader>x", ":q!<cr>")
+map("n", "<leader>Q", ":wq<cr>")
 
 -- Telescope
-map("n", "<C-f>", "<cmd>Telescope find_files<cr>", opts)
-map("n", "<C-t>", "<cmd>Telescope live_grep<cr>", opts)
+map("n", "<C-f>", "<cmd>Telescope find_files<cr>")
+map("n", "<C-t>", "<cmd>Telescope live_grep<cr>")
 
 -- Backspace turn off search highlitning
-map("n", "<BS>", ":nohlsearch<cr>", opts)
+map("n", "<BS>", ":nohlsearch<cr>")
 
 -- Nvimtree
-map("n", "<C-n>", ":Neotree toggle<cr>", opts)
+map("n", "<C-n>", ":Neotree toggle<cr>")
 
 -- Null-ls
-map("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<cr>", opts)
-map("v", "<leader>f", "<cmd>lua vim.lsp.buf.range_format()<cr>", opts)
-
--- -- Bufferline
--- keymap("n", "<leader><tab>", ":BufferLineCycleNext<cr>", opts)
--- keymap("n", "<leader><Backspace>", ":BufferLineCyclePrev<cr>", opts)
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<cr>")
+map("v", "<leader>f", "<cmd>lua vim.lsp.buf.range_format()<cr>")
 
 -- ToggleTerm
-map({ "n", "t" }, "<C-s>", "<cmd>ToggleTerm<cr>", opts)
-map("n", "<M-g>", "<cmd>lua _LAZYGIT_TOGGLE()<cr>", opts)
+map({ "n", "t" }, "<C-s>", "<cmd>ToggleTerm<cr>")
+map("n", "<M-g>", "<cmd>lua _LAZYGIT_TOGGLE()<cr>")
 
 -- WhichKey
--- keymap("n", "<C-_>", "<cmd>WhichKey<cr>", opts)
-map({ "n", "i" }, "<C-_>", "<cmd>WhichKey<cr>", opts)
+map({ "n", "i" }, "<C-_>", "<cmd>WhichKey<cr>")
