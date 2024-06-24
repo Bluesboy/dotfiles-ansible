2d192d18ba4e57f8f747186b57e867c374da5e63
Bluesboy <youngreg@ya.ru>
2024-06-25 02:58:26 +0500

    Disable sidebars

--- config/nvim/lua/plugins/github-colorscheme.lua
+++ config/nvim/lua/plugins/github-colorscheme.lua
@@ -8,9 +8,9 @@ return {
           -- Compiled file's destination location
           hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
           hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-          transparent = false, -- Disable setting background
+          transparent = true, -- Disable setting background
           terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false, -- Non focused panes set to alternative background
+          dim_inactive = true, -- Non focused panes set to alternative background
           module_default = true, -- Default enable value for modules
           styles = { -- Style to be applied to different syntax groups
             comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
@@ -32,7 +32,7 @@ return {
           darken = {
             floats = false,
             sidebars = {
-              enabled = true,
+              enabled = false,
               list = {},
             },
           },
