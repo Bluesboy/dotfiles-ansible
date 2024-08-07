6235d4ca66f95a86b3a7cb1ed213708d98bc5c66
Bluesboy <youngreg@ya.ru>
2024-08-08 03:47:21 +0500

    Configure transparency in NeoVim

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -51,8 +51,8 @@ return {
   {
     "xiyaowong/transparent.nvim",
     config = function()
-      -- require("transparent").clear_prefix("NeoTree")
-      require("transparent").clear_prefix("lualine")
+      require("transparent").clear_prefix("NeoTree")
+      require("transparent").clear_prefix("BufferLine")
     end,
   },
   {
