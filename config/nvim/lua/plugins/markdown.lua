24ced378bf764502d729e3cc8b172c70e3026416
Bluesboy <youngreg@ya.ru>
2024-10-14 06:09:52 +0500

    Format config

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,10 @@
 return {
   {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      ft = { "markdown" },
-      build = function() vim.fn["mkdp#util#install"]() end,
-  }
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
