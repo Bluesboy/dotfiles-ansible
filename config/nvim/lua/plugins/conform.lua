e275750a50cceb298b67ebfb9dc1cad1c447293e
Bluesboy <youngreg@ya.ru>
2025-08-22 04:26:47 +0500

    Add plugins for Neovim

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -15,26 +15,27 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {
-          yaml = true,
-          c = true,
-          cpp = true,
-          go = true,
-        }
-
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 200,
-            lsp_format = "fallback",
-          }
-        end
-      end,
+      format_on_save = false,
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = {
+      --     yaml = true,
+      --     c = true,
+      --     cpp = true,
+      --     go = true,
+      --   }
+      --
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     return nil
+      --   else
+      --     return {
+      --       timeout_ms = 200,
+      --       lsp_format = "fallback",
+      --     }
+      --   end
+      -- end,
       formatters = {
         yamlfmt = {
           prepend_args = {
