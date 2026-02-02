db5d4c63c69b191278f05a66227021fcc3ce8777
Bluesboy <youngreg@ya.ru>
2026-02-03 01:38:23 +0500

    Install time-machine

---
+++ config/nvim/lua/plugins/time-machine.lua
@@ -0,0 +1,9 @@
+return {
+ "y3owk1n/time-machine.nvim",
+ version = "*",
+ opts = {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+ }
+}
