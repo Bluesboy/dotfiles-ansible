dd1fbc8abc1301d8647370a7b17f082113c1f11a
Bluesboy <youngreg@ya.ru>
2026-03-08 04:18:14 +0500

    Install `lazygit.nvim`

--- config/nvim/lua/plugins/neoscroll.lua
+++ config/nvim/lua/plugins/neoscroll.lua
@@ -14,7 +14,7 @@ return {
         "zz",
         "zb",
       },
-      hide_cursor = true, -- Hide cursor while scrolling
+      hide_cursor = false, -- Hide cursor while scrolling
       stop_eof = true, -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
