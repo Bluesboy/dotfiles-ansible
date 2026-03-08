c8844bbac956361e14c1214382e91b07cca7048f
Bluesboy <youngreg@ya.ru>
2026-03-08 07:22:20 +0500

    Install `dial.nvim`

---
+++ config/nvim/lua/plugins/dial.lua
@@ -0,0 +1,16 @@
+return {
+  "monaqa/dial.nvim",
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.constant.alias.bool,
+        augend.integer.alias.decimal_int,
+        augend.date.alias["%d.%m.%Y"],
+        augend.semver.alias.semver,
+      },
+    })
+    vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+    vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+  end,
+}
