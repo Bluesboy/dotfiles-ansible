e42af765b6055a14915dde3bb88f70bb95f39570
Bluesboy <youngreg@ya.ru>
2026-03-11 04:36:00 +0500

    Remove transparent.nvim and turn on transparency in Catppuccin

--- config/nvim/lua/plugins/ui/catppuccin.lua
+++ config/nvim/lua/plugins/ui/catppuccin.lua
@@ -9,7 +9,7 @@ return {
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = false,
+      transparent_background = true,
       show_end_of_buffer = false,
       term_colors = false,
       dim_inactive = {
