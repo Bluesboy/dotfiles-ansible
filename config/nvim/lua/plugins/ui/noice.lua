4941da77134903f115e39f2a9a927a236984175c
Bluesboy <youngreg@ya.ru>
2026-03-12 02:32:13 +0500

    Configure noice.nvim

--- config/nvim/lua/plugins/ui/noice.lua
+++ config/nvim/lua/plugins/ui/noice.lua
@@ -2,7 +2,22 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
@@ -18,12 +33,12 @@ return {
       timeout = 3000,
       max_width = 80,
       icons = {
-        ERROR = "",
-        WARN  = "",
-        INFO  = "󰋼",
-        DEBUG = "",
+        ERROR = "",
+        WARN = "",
+        INFO = "󰋼",
+        DEBUG = "",
         TRACE = "✎",
       },
     },
-  }
+  },
 }
