bd72d76af708864d30ce9fb02ba7f71753ffaccf
Bluesboy <youngreg@ya.ru>
2026-03-12 02:30:48 +0500

    Configure `lualine.nvim`

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -8,14 +8,15 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_c = { { "filename", path = 3 } },
+      lualine_x = { "encoding", "fileformat", "filetype", "lsp_status" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
     extensions = {
       "lazy",
       "neo-tree",
+      "trouble",
     },
   },
 }
