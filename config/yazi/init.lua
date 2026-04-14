4795a89cd66bf419d84e8b0d28f03580ea603de9
Bluesboy <youngreg@ya.ru>
2026-04-15 02:43:28 +0500

    Install `eza-preview` for Yazi

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -75,3 +75,38 @@ require("projects"):setup({
     level = "info",
   },
 })
+
+require("eza-preview"):setup({
+  -- Set the tree preview to be default (default: true)
+  default_tree = true,
+
+  -- Directory depth level for tree preview (default: 3)
+  level = 3,
+
+  -- Show file icons
+  icons = true,
+
+  -- Follow symlinks when previewing directories (default: true)
+  follow_symlinks = true,
+
+  -- Show target file info instead of symlink info (default: false)
+  dereference = false,
+
+  -- Show hidden files (default: true)
+  all = true,
+
+  -- Ignore files matching patterns (default: {})
+  -- ignore_glob = "*.log"
+  -- ignore_glob = { "*.tmp", "node_modules", ".git", ".DS_Store" }
+  -- SEE: https://www.linuxjournal.com/content/pattern-matching-bash to learn about glob patterns
+  ignore_glob = {},
+
+  -- Ignore files mentioned in '.gitignore'  (default: true)
+  git_ignore = false,
+
+  -- Show git status (default: false)
+  git_status = false,
+})
+
+-- Or use default settings
+require("eza-preview"):setup({})
